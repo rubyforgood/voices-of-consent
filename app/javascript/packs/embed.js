84467a2c20159403208f15be9070ca3183afc319
Adam Bachman <adam.bachman@gmail.com>
2019-07-23 18:56:03 -0400

    i am a huge idiot and you can't require a css pack if you never created a css pack :P

--- app/javascript/packs/embed.js
+++ app/javascript/packs/embed.js
@@ -2,3 +2,4 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 
 // Code for the contact form component starts here.
+import 'embed/behavior'
---
+++ app/javascript/src/embed/behavior.js
@@ -0,0 +1,3 @@
+import 'embed/style.scss'
+
+console.log('this is embedded in an iframe!')
