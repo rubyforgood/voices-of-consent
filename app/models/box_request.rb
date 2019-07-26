90d8a4ec6a05ec40e82753aa7b16adbf3e293959
Paige Finkelstein <paigelfink@gmail.com>
2019-07-26 13:28:29 -0400

    Add tags

--- app/models/box_request.rb
+++ app/models/box_request.rb
@@ -6,4 +6,6 @@ class BoxRequest < ApplicationRecord
   validates :question_re_affect, presence: true
   validates :question_re_referral_source, presence: true
   validates :question_re_current_situation, presence: true
+
+  acts_as_taggable
 end
