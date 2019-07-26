756436acd5c7996db66d5212d971b971c5b025e5
Gabriel Zurita <gzurita@optoro.com>
2019-07-26 12:47:34 -0500

    add volunteer validations

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,2 +1,15 @@
+# frozen_string_literal: true
+
 class Volunteer < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :street_address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip, presence: true
+  validates :ok_to_email, inclusion: { in: [true, false] }
+  validates :ok_to_text, inclusion: { in: [true, false] }
+  validates :ok_to_call, inclusion: { in: [true, false] }
+  validates :ok_to_mail, inclusion: { in: [true, false] }
+  validates :underage, inclusion: { in: [true, false] }
 end
