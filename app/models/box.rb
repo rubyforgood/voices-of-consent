cfee295c4f72e6f8ffb8d1a0709dbe3b83e79a8c
Svetlana Vileshina <svileshina@gmail.com>
2019-07-26 16:50:02 -0400

    add to Box and User model

--- app/models/box.rb
+++ app/models/box.rb
@@ -1,8 +1,9 @@
 class Box < ApplicationRecord
   belongs_to :box_request
-  belongs_to :designed_by
-  belongs_to :design_reviewed_by
-  belongs_to :assembled_by
-  belongs_to :shipped_by
-  belongs_to :shipping_payment
+  belongs_to :designed_by, optional: true, class_name: "User", foreign_key: :designed_by_id, inverse_of: :boxes_as_designer
+  belongs_to :design_reviewed_by, optional: true, class_name: "User", foreign_key: :design_reviewed_by_id, inverse_of: :boxes_as_design_reviewer
+  belongs_to :assembled_by, optional: true, class_name: "User", foreign_key: :assembled_by_id, inverse_of: :boxes_as_assembler
+  belongs_to :shipped_by, optional: true, class_name: "User", foreign_key: :shipped_by_id, inverse_of: :boxes_as_shipper
+  # add to Purchase Model when made
+  # belongs_to :shipping_payment, optional: true, class_name: "Purchase", foreign_key: :shipping_payment_id, inverse_of: :payment_for_shipment
 end
