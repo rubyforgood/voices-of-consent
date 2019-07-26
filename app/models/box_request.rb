95452ad10b846a4ec458955e586dbe7779a0ebf1
Paige Finkelstein <paigelfink@gmail.com>
2019-07-26 13:19:19 -0400

    Add validations and specs

--- app/models/box_request.rb
+++ app/models/box_request.rb
@@ -1,3 +1,9 @@
 class BoxRequest < ApplicationRecord
   belongs_to :requester
+
+  validates :requester, presence: true
+  validates :summary, presence: true
+  validates :question_re_affect, presence: true
+  validates :question_re_referral_source, presence: true
+  validates :question_re_current_situation, presence: true
 end
