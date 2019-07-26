a5ff536d0d1e4dce1fb57c8d5907282d1620d9bf
Paige Finkelstein <paigelfink@gmail.com>
2019-07-26 13:36:35 -0400

    [12] Requester crud (#37)
    * Closes Issue #12 
    
    * Add Requester model
    
    * Add some Requestor specs
    
    * Add email address

---
+++ spec/models/requester_spec.rb
@@ -0,0 +1,68 @@
+require 'rails_helper'
+
+RSpec.describe Requester, :type => :model do
+ it "is valid with valid attributes" do
+   r = Requester.new(first_name: "Jane", last_name: "Doe", email: "jane@email.com", street_address: "122 Boggie Woogie Avenue", city: "Fairfax", state: "VA", zip: "22030", ok_to_email: true, ok_to_text: false, ok_to_call: false, ok_to_mail: true, underage: false)
+   expect(r).to be_valid
+ end
+
+ it "requires first_name" do
+   r = Requester.new(last_name: "Doe", email: "jane@email.com", street_address: "122 Boggie Woogie Avenue", city: "Fairfax", state: "VA", zip: "22030", ok_to_email: true, ok_to_text: false, ok_to_call: false, ok_to_mail: true, underage: false)
+   expect(r).to_not be_valid
+ end
+
+ it "requires last_name" do
+   r = Requester.new(first_name: "Jane", email: "jane@email.com", street_address: "122 Boggie Woogie Avenue", city: "Fairfax", state: "VA", zip: "22030", ok_to_email: true, ok_to_text: false, ok_to_call: false, ok_to_mail: true, underage: false)
+   expect(r).to_not be_valid
+ end
+
+ it "requires email" do
+   r = Requester.new(first_name: "Jane", last_name: "Doe", street_address: "122 Boggie Woogie Avenue", city: "Fairfax", state: "VA", zip: "22030", ok_to_email: true, ok_to_text: false, ok_to_call: false, ok_to_mail: true, underage: false)
+   expect(r).to_not be_valid
+ end
+
+ it "requires street_address" do
+   r = Requester.new(first_name: "Jane", last_name: "Doe", email: "jane@email.com", city: "Fairfax", state: "VA", zip: "22030", ok_to_email: true, ok_to_text: false, ok_to_call: false, ok_to_mail: true, underage: false)
+   expect(r).to_not be_valid
+ end
+
+ it "requires city" do
+   r = Requester.new(first_name: "Jane", last_name: "Doe", email: "jane@email.com", street_address: "122 Boggie Woogie Avenue", state: "VA", zip: "22030", ok_to_email: true, ok_to_text: false, ok_to_call: false, ok_to_mail: true, underage: false)
+   expect(r).to_not be_valid
+ end
+
+ it "requires state" do
+   r = Requester.new(first_name: "Jane", last_name: "Doe", email: "jane@email.com", street_address: "122 Boggie Woogie Avenue", city: "Fairfax", zip: "22030", ok_to_email: true, ok_to_text: false, ok_to_call: false, ok_to_mail: true, underage: false)
+   expect(r).to_not be_valid
+ end
+
+ it "requires zip" do
+   r = Requester.new(first_name: "Jane", last_name: "Doe", email: "jane@email.com", street_address: "122 Boggie Woogie Avenue", city: "Fairfax", state: "VA", ok_to_email: true, ok_to_text: false, ok_to_call: false, ok_to_mail: true, underage: false)
+   expect(r).to_not be_valid
+ end
+
+ it "requires ok_to_text" do
+   r = Requester.new(first_name: "Jane", last_name: "Doe", email: "jane@email.com", street_address: "122 Boggie Woogie Avenue", city: "Fairfax", state: "VA", zip: "22030", ok_to_email: true, ok_to_call: false, ok_to_mail: true, underage: false)
+   expect(r).to_not be_valid
+ end
+
+ it "requires ok_to_call" do
+   r = Requester.new(first_name: "Jane", last_name: "Doe", email: "jane@email.com", street_address: "122 Boggie Woogie Avenue", city: "Fairfax", state: "VA", zip: "22030", ok_to_email: true, ok_to_text: false, ok_to_mail: true, underage: false)
+   expect(r).to_not be_valid
+ end
+
+ it "requires ok_to_mail" do
+   r = Requester.new(first_name: "Jane", last_name: "Doe", email: "jane@email.com", street_address: "122 Boggie Woogie Avenue", city: "Fairfax", state: "VA", zip: "22030", ok_to_email: true, ok_to_text: false, ok_to_call: false, underage: false)
+   expect(r).to_not be_valid
+ end
+
+ it "requires ok_to_email" do
+   r = Requester.new(first_name: "Jane", last_name: "Doe", email: "jane@email.com", street_address: "122 Boggie Woogie Avenue", city: "Fairfax", state: "VA", zip: "22030", ok_to_text: false, ok_to_call: false, ok_to_mail: true, underage: false)
+   expect(r).to_not be_valid
+ end
+
+ it "requires underage" do
+   r = Requester.new(first_name: "Jane", last_name: "Doe", email: "jane@email.com", street_address: "122 Boggie Woogie Avenue", city: "Fairfax", state: "VA", zip: "22030", ok_to_email: true, ok_to_text: false, ok_to_call: false, ok_to_mail: true)
+   expect(r).to_not be_valid
+ end
+end
---
+++ test/system/requesters_test.rb
@@ -0,0 +1,69 @@
+require "application_system_test_case"
+
+class RequestersTest < ApplicationSystemTestCase
+  setup do
+    @requester = requesters(:one)
+  end
+
+  test "visiting the index" do
+    visit requesters_url
+    assert_selector "h1", text: "Requesters"
+  end
+
+  test "creating a Requester" do
+    visit requesters_url
+    click_on "New Requester"
+
+    fill_in "City", with: @requester.city
+    fill_in "County", with: @requester.county
+    fill_in "First name", with: @requester.first_name
+    fill_in "Last name", with: @requester.last_name
+    fill_in "Email", with: @requester.email
+    fill_in "Ok to call", with: @requester.ok_to_call
+    fill_in "Ok to email", with: @requester.ok_to_email
+    fill_in "Ok to mail", with: @requester.ok_to_mail
+    fill_in "Ok to text", with: @requester.ok_to_text
+    fill_in "Phone", with: @requester.phone
+    fill_in "State", with: @requester.state
+    fill_in "Street address", with: @requester.street_address
+    fill_in "Underage", with: @requester.underage
+    fill_in "Zip", with: @requester.zip
+    click_on "Create Requester"
+
+    assert_text "Requester was successfully created"
+    click_on "Back"
+  end
+
+  test "updating a Requester" do
+    visit requesters_url
+    click_on "Edit", match: :first
+
+    fill_in "City", with: @requester.city
+    fill_in "County", with: @requester.county
+    fill_in "First name", with: @requester.first_name
+    fill_in "Last name", with: @requester.last_name
+    fill_in "Email", with: @requester.email
+    fill_in "Ok to call", with: @requester.ok_to_call
+    fill_in "Ok to email", with: @requester.ok_to_email
+    fill_in "Ok to mail", with: @requester.ok_to_mail
+    fill_in "Ok to text", with: @requester.ok_to_text
+    fill_in "Phone", with: @requester.phone
+    fill_in "State", with: @requester.state
+    fill_in "Street address", with: @requester.street_address
+    fill_in "Underage", with: @requester.underage
+    fill_in "Zip", with: @requester.zip
+    click_on "Update Requester"
+
+    assert_text "Requester was successfully updated"
+    click_on "Back"
+  end
+
+  test "destroying a Requester" do
+    visit requesters_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "Requester was successfully destroyed"
+  end
+end
