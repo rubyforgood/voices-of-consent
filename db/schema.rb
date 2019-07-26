19966e79e9c696e7479e597bfdced8189ddfac97
abudri <17362519+abudri@users.noreply.github.com>
2019-07-26 16:03:31 -0400

    [#18]InventoryType CRUD (#58)
    * [#18]InventoryType CRUD
    
    * [#18] fix migration schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_26_175433) do
+ActiveRecord::Schema.define(version: 2019_07_26_180805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 2019_07_26_175433) do
   create_table "inventory_tallies", force: :cascade do |t|
     t.string "additional_location_info"
     t.integer "cached_quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "inventory_types", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
---
+++ test/models/inventory_type_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class InventoryTypeTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
