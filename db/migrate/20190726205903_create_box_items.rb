c78bbc7ee505c00339ca8317dd732dddb0cae955
Gabriel Zurita <gzurita@optoro.com>
2019-07-26 16:11:18 -0500

    issue-57: update migrations and re-migrate

--- db/migrate/20190726205903_create_box_items.rb
+++ db/migrate/20190726205903_create_box_items.rb
@@ -1,7 +1,7 @@
 class CreateBoxItems < ActiveRecord::Migration[5.2]
   def change
     create_table :box_items do |t|
-      t.integer :box_id,nindex: true, null: false
+      t.integer :box_id, index: true, null: false
       t.integer :inventory_adjustment_id, index: true, null: false
       t.integer :researched_by_id, index: true, null: false
       t.boolean :added_to_box
