f932446c1bc43c7b61987eeb47f160235fda0a39
Abdullah Budri <17362519+abudri@users.noreply.github.com>
2019-07-26 15:09:43 -0400

    [#18] fix migration schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_07_26_180805) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +30,13 @@ ActiveRecord::Schema.define(version: 2019_07_26_180805) do
   create_table "inventory_tallies", force: :cascade do |t|
     t.string "additional_location_info"
     t.integer "cached_quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "inventory_types", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -118,13 +126,6 @@ ActiveRecord::Schema.define(version: 2019_07_26_180805) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  create_table "inventory_types", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "volunteers", force: :cascade do |t|
