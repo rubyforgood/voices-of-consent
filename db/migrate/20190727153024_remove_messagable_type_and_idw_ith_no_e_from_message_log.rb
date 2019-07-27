f279ea7ae400240c25c6e29fbed943d2943772a3
Jamie Gaskins <jgaskins@gmail.com>
2019-07-27 15:31:48 -0400

    Delay emails (#110)
    * Push email out to Sidekiq
    
    This way, we aren't sending the email during the request/response cycle,
    delaying (and possibly breaking) the HTTP response. The email will send
    whenever we have capacity for it and if it breaks it doesn't interrupt
    the HTTP response. It will be retried through Sidekiq's retry mechanism.
    
    This commit also moves the check for `ok_to_email` inside a check for a
    saved record. We've already run `volunteer.save` so we don't need to run
    it again.
    
    * Use predicate method
    
    Boolean attributes are ideally accessed via predicates to indicate that
    they're being used as booleans.
    
    * yo, how did this even happen?

--- db/migrate/20190727153024_remove_messagable_type_and_idw_ith_no_e_from_message_log.rb
+++
@@ -1,6 +0,0 @@
-class RemoveMessagableTypeAndIdwIthNoEFromMessageLog < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :message_logs, :messageable_type, :string
-    remove_column :message_logs, :messageable_id, :integer
-  end
-end
