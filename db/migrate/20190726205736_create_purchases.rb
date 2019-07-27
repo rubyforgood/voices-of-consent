ad3b38ae40324d309509a40e58ba7b7fcf7ff361
Svetlana Vileshina <svileshina@gmail.com>
2019-07-27 10:31:01 -0400

    purchase model

--- db/migrate/20190726205736_create_purchases.rb
+++ db/migrate/20190726205736_create_purchases.rb
@@ -1,10 +1,10 @@
 class CreatePurchases < ActiveRecord::Migration[5.2]
   def change
     create_table :purchases do |t|
-      t.references :location, foreign_key: true
+      t.references :location, foreign_key: true, null: true, index: true
       t.float :total_price
-      t.references :purchased_by, foreign_key: true
-      t.references :reimbursed_by, foreign_key: true
+      t.references :purchased_by
+      t.references :reimbursed_by
       t.string :reimbursement_check_number
       t.string :reimbursement_status
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_26_180805) do
+ActiveRecord::Schema.define(version: 2019_07_26_205736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,25 @@ ActiveRecord::Schema.define(version: 2019_07_26_180805) do
     t.datetime "updated_at", null: false
     t.integer "requester_id"
     t.index ["requester_id"], name: "index_box_requests_on_requester_id"
+  end
+
+  create_table "boxes", force: :cascade do |t|
+    t.bigint "box_request_id", null: false
+    t.bigint "designed_by_id"
+    t.bigint "design_reviewed_by_id"
+    t.bigint "assembled_by_id"
+    t.bigint "shipped_by_id"
+    t.bigint "shipping_payment_id"
+    t.datetime "shipped_at"
+    t.string "shipment_tracking_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["assembled_by_id"], name: "index_boxes_on_assembled_by_id"
+    t.index ["box_request_id"], name: "index_boxes_on_box_request_id"
+    t.index ["design_reviewed_by_id"], name: "index_boxes_on_design_reviewed_by_id"
+    t.index ["designed_by_id"], name: "index_boxes_on_designed_by_id"
+    t.index ["shipped_by_id"], name: "index_boxes_on_shipped_by_id"
+    t.index ["shipping_payment_id"], name: "index_boxes_on_shipping_payment_id"
   end
 
   create_table "inventory_tallies", force: :cascade do |t|
@@ -70,6 +89,20 @@ ActiveRecord::Schema.define(version: 2019_07_26_180805) do
     t.datetime "updated_at", null: false
     t.index ["location_id"], name: "index_meetings_on_location_id"
     t.index ["meeting_type_id"], name: "index_meetings_on_meeting_type_id"
+  end
+
+  create_table "purchases", force: :cascade do |t|
+    t.bigint "location_id"
+    t.float "total_price"
+    t.bigint "purchased_by_id"
+    t.bigint "reimbursed_by_id"
+    t.string "reimbursement_check_number"
+    t.string "reimbursement_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["location_id"], name: "index_purchases_on_location_id"
+    t.index ["purchased_by_id"], name: "index_purchases_on_purchased_by_id"
+    t.index ["reimbursed_by_id"], name: "index_purchases_on_reimbursed_by_id"
   end
 
   create_table "requesters", force: :cascade do |t|
@@ -148,6 +181,8 @@ ActiveRecord::Schema.define(version: 2019_07_26_180805) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "boxes", "box_requests"
   add_foreign_key "meetings", "locations"
   add_foreign_key "meetings", "meeting_types"
+  add_foreign_key "purchases", "locations"
 end
