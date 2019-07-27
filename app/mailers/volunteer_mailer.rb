16d8d3ee51f909aa3368a6b98a3a1c8ecb220571
Miranda Wang <mirandawang94@gmail.com>
2019-07-27 10:15:40 -0400

    Creating new method for the box request email and the ERB file (#78)

--- app/mailers/volunteer_mailer.rb
+++ app/mailers/volunteer_mailer.rb
@@ -3,4 +3,9 @@ class VolunteerMailer < ApplicationMailer
         @volunteer = volunteer
         mail(to: User.find(@volunteer.user_id).email, subject: 'Welcome to Voices of Consent!')
     end
+
+    def box_request_email(volunteer)
+        @volunteer = volunteer
+        mail(to: User.find(@volunteer.user_id).email, subject: 'A Box Request has been sent')
+    end
 end
