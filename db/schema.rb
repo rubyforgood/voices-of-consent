68215a804a26b32176136cdaad01dea9916efa0d
abudri <17362519+abudri@users.noreply.github.com>
2019-07-27 10:17:24 -0400

    [#18]Add Foreign keys InventoryTally (#74)
    * [#18]InventoryType CRUD
    
    * [#18]Add Foreign keys InventoryTally

--- db/schema.rb
+++ db/schema.rb
@@ -80,6 +80,10 @@ ActiveRecord::Schema.define(version: 2019_07_26_210045) do
     t.integer "cached_quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "inventory_type_id"
+    t.bigint "storage_location_id"
+    t.index ["inventory_type_id"], name: "index_inventory_tallies_on_inventory_type_id"
+    t.index ["storage_location_id"], name: "index_inventory_tallies_on_storage_location_id"
   end
 
   create_table "inventory_types", force: :cascade do |t|
@@ -96,6 +100,13 @@ ActiveRecord::Schema.define(version: 2019_07_26_210045) do
     t.string "state"
     t.string "zip"
     t.integer "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "inventory_types", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -197,6 +208,7 @@ ActiveRecord::Schema.define(version: 2019_07_26_210045) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "inventory_tallies", "locations", column: "storage_location_id"
   add_foreign_key "core_box_items", "abuse_types"
   add_foreign_key "core_box_items", "inventory_types"
   add_foreign_key "meetings", "locations"
