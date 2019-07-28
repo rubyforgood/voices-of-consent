8c85bcbd15f7bc0578b91eabc37e050632f91e51
bransynluther <bransynluther@gmail.com>
2019-07-28 11:22:55 -0400

    Add require login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery
   layout 'application_full'
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || root_path
+  end
 end
