f43105b6000a6ca384148d3da8242c688a4a1b22
Miranda Wang <mirandawang94@gmail.com>
2019-07-28 08:14:06 -0400

    Adding email templates for each step and todos in mailer actions

--- app/mailers/volunteer_mailer.rb
+++ app/mailers/volunteer_mailer.rb
@@ -8,4 +8,25 @@ class VolunteerMailer < ApplicationMailer
         @volunteer = volunteer
         mail(to: User.find(@volunteer.user_id).email, subject: 'A Box Request has been sent')
     end
+
+    def box_design_email(box_id)
+        #todo: query all volunteers that have permission to design boxes
+        # @volunteers.each do |volunteer|
+        # mail(to:)
+        # pass url for the box design page for that box id
+    end
+
+    def box_assembly_email(box_id)
+        #todo: query all volunteers that have permission to assemble boxes
+        # @volunteers.each do |volunteer|
+        # mail(to:)
+        # pass url for the box assembly page for that box id
+    end
+
+    def box_shipping_email(box_id)
+        #todo: query all volunteers that have permission to ship boxes
+        # @volunteers.each do |volunteer|
+        # mail(to:)
+        # pass url for the box shipping page for that box id
+    end
 end
