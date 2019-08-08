1aed87ed18ebe111335e195753362a1b105d22a5
maebeale <maebeale@gmail.com>
2019-08-08 18:03:46 -0400

    MessageableType needs to be a valid model name

--- spec/views/message_logs/index.html.erb_spec.rb
+++ spec/views/message_logs/index.html.erb_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "message_logs/index", type: :view do
   before(:each) do
     assign(:message_logs, [
       MessageLog.create!(
-        :messageable_type => "messageable Type",
+        :messageable_type => "BoxRequest",
         :messageable_id => 2,
         :content => "MyText",
         :delivery_type => 3,
@@ -13,7 +13,7 @@ RSpec.describe "message_logs/index", type: :view do
         :sent_by_id => 5
       ),
       MessageLog.create!(
-        :messageable_type => "messageable Type",
+        :messageable_type => "BoxRequest",
         :messageable_id => 2,
         :content => "MyText",
         :delivery_type => 3,
@@ -26,7 +26,7 @@ RSpec.describe "message_logs/index", type: :view do
 
   it "renders a list of message_logs" do
     render
-    assert_select "tr>td", :text => "messageable Type".to_s, :count => 2
+    assert_select "tr>td", :text => "BoxRequest".to_s, :count => 2
     assert_select "tr>td", :text => 2.to_s, :count => 2
     assert_select "tr>td", :text => "MyText".to_s, :count => 2
     assert_select "tr>td", :text => 3.to_s, :count => 2
