5a71b7a1ec2e7f98274424d5a429329f9bd7df2a
bransynluther <bransynluther@gmail.com>
2019-08-10 14:24:31 -0400

    Include after warden test reset! hook
    This line prevents any unexpected state data pertaining to a user from
    sticking around and possibly causing errors.

--- spec/requests/attendances_spec.rb
+++ spec/requests/attendances_spec.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Attendances", type: :request do
+
   describe "GET /attendances" do
 
     before do
       user = FactoryBot.create(:user)
       login_as(user, :scope => :user)
     end
+
+    after { Warden.test_reset! }
 
     it "responds with the correct html content" do
       get attendances_path
