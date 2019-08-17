b2787f3f90572fe73d750238d02af5fba9ae9f6a
MxOliver <mxoliverleigh@gmail.com>
2019-08-17 11:53:31 -0400

    add relevant tests

--- spec/models/message_log_spec.rb
+++ spec/models/message_log_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe MessageLog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+ subject(:message_log) { MessageLog.new(content: "Content Test", messageable_type: "type", messageable_id: 11111, message_channel: "email", message_type: "followup_email", subject_line: "subject") }
+
+ describe "attributes" do
+  it "responds to attributes content messageable_type subject_line message_channel and message_type" do
+    expect(message_log).to have_attributes(content: "Content Test", messageable_type: "type", messageable_id: 11111, message_channel: "email", message_type: "followup_email", subject_line: "subject")
+  end
+end
+
+
 end
