4134c33b636e107c0345f14ab6e19b5bdb16bbf6
Kate Studwell <katestud@github.com>
2019-08-28 19:03:34 -0400

    Fix message logs view specs

--- spec/views/message_logs/index.html.erb_spec.rb
+++ spec/views/message_logs/index.html.erb_spec.rb
@@ -1,36 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe "message_logs/index", type: :view do
+  let(:sent_to) { create(:user) }
+  let(:sent_by) { create(:user) }
+
   before(:each) do
-    messageable = create(:box_request)
-    create(:message_log,
-           messageable_type: "BoxRequest",
-           messageable_id: messageable.id,
-           content: "MyText",
-           delivery_type: 3,
-           delivery_status: "Delivery Status",
-           sent_to_id: 4,
-           sent_by_id: 5
-    )
-    create(:message_log,
-           messageable_type: "BoxRequest",
-           messageable_id: messageable,
-           content: "MyText",
-           delivery_type: 3,
-           delivery_status: "Delivery Status",
-           sent_to_id: 4,
-           sent_by_id: 5
-    )
+    @message_logs = [
+      create(:message_log,
+            messageable: create(:box_request),
+            content: "MyText",
+            delivery_type: 3,
+            delivery_status: "Delivery Status",
+            sent_to: sent_to,
+            sent_by: sent_by
+      ),
+      create(:message_log,
+            messageable: create(:box_request),
+            content: "MyText",
+            delivery_type: 3,
+            delivery_status: "Delivery Status",
+            sent_to: sent_to,
+            sent_by: sent_by
+      )
+    ]
   end
 
   it "renders a list of message_logs" do
     render
     assert_select "tr>td", :text => "BoxRequest".to_s, :count => 2
-    assert_select "tr>td", :text => 2.to_s, :count => 2
     assert_select "tr>td", :text => "MyText".to_s, :count => 2
-    assert_select "tr>td", :text => 3.to_s, :count => 2
     assert_select "tr>td", :text => "Delivery Status".to_s, :count => 2
-    assert_select "tr>td", :text => 4.to_s, :count => 2
-    assert_select "tr>td", :text => 5.to_s, :count => 2
+    assert_select "tr>td", :text => sent_to.id.to_s, :count => 2
+    assert_select "tr>td", :text => sent_by.id.to_s, :count => 2
   end
 end
--- spec/views/message_logs/show.html.erb_spec.rb
+++ spec/views/message_logs/show.html.erb_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "message_logs/show", type: :view do
+  let(:sent_to) { create(:user) }
+  let(:sent_by) { create(:user) }
+
   before(:each) do
     messageable = create(:box_request)
     @message_log = create(:message_log,
@@ -9,19 +12,18 @@ RSpec.describe "message_logs/show", type: :view do
                           content: "MyText",
                           delivery_type: 3,
                           delivery_status: "Delivery Status",
-                          sent_to_id: 4,
-                          sent_by_id: 5
+                          sent_to: sent_to,
+                          sent_by: sent_by
     )
   end
 
   it "renders attributes in <p>" do
     render
-    expect(rendered).to match(/messageable Type/)
-    expect(rendered).to match(/2/)
+    expect(rendered).to match(/messageable type/)
     expect(rendered).to match(/MyText/)
-    expect(rendered).to match(/3/)
+    expect(rendered).to match(/phone/)
     expect(rendered).to match(/Delivery Status/)
-    expect(rendered).to match(/4/)
-    expect(rendered).to match(/5/)
+    expect(rendered).to match(Regexp.new(sent_to.id.to_s))
+    expect(rendered).to match(Regexp.new(sent_by.id.to_s))
   end
 end
