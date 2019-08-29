7a0562ddaefaa756de7ba31ce7b1cb9eb22f6279
Kate Studwell <katestud@github.com>
2019-08-28 22:59:54 -0400

    Add relevant attrs to attendances/show spec

--- spec/views/attendances/show.html.erb_spec.rb
+++ spec/views/attendances/show.html.erb_spec.rb
@@ -2,15 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "attendances/show", type: :view do
   before(:each) do
-
     @attendance = create(:attendance)
   end
 
   it "renders attributes in <p>" do
     render
-    expect(rendered).to match(//)
-    expect(rendered).to match(//)
-    expect(rendered).to match(/Special Duties/)
-    expect(rendered).to match(/false/)
+    expect(rendered).to match(/Special duties/)
+    expect(rendered).to match(/Completed hours/)
   end
 end
