63be1eca3a678d654999113853070936b5293270
maebeale <maebeale@gmail.com>
2019-10-05 10:46:05 -0400

    Restructure DeliveryType enum in MessageLog (and fix indentation)

--- app/models/message_log.rb
+++ app/models/message_log.rb
@@ -1,6 +1,12 @@
 class MessageLog < ApplicationRecord
-    belongs_to :messageable, :polymorphic => true
-    belongs_to :sent_by, class_name: "User", foreign_key: :sent_by_id, inverse_of: :message_logs_sent
-    belongs_to :sent_to, class_name: "User", foreign_key: :sent_to_id, inverse_of: :message_logs_received
-    enum delivery_type: [:text, :autoemail, :email, :phone]
+  belongs_to :messageable, :polymorphic => true
+  belongs_to :sent_by, class_name: "User", foreign_key: :sent_by_id, inverse_of: :message_logs_sent
+  belongs_to :sent_to, class_name: "User", foreign_key: :sent_to_id, inverse_of: :message_logs_received
+
+  enum delivery_type: {
+    text: 0,
+    autoemail: 1,
+    email: 2,
+    phone: 3,
+  }
 end
