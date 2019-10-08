9ed43a9939f603d2dd19f337192af1df5227adf2
Sherilyn Moline <smoline@gmail.com>
2019-10-08 19:25:48 -0400

    select options and inputs update state

--- app/javascript/src/components/message-log-form/MessageLogForm.jsx
+++ app/javascript/src/components/message-log-form/MessageLogForm.jsx
@@ -13,15 +13,26 @@ class MessageLogForm extends React.Component {
   constructor(props) {
     super(props),
     this.state = {
-      messageLog: {
-        messageableType: "",
-        messageableIds: []
+      isFormSubmitting: false,
+      message_log: {
+        messageable_type: "",
+        messageable_id: "",
+        content: "",
+        delivery_type: "",
+        delivery_status: "",
+        sent_to_id: "",
+        sent_by_id: ""
       },
     }
+  }
+
+  handleChange = (event) => {
+    this.setState({ message_log: { ...this.state.message_log, [event.target.name]: event.target.value } });
   }
   
   // TODO: replace messageable id dropdown with real data once we can fetch it from the db
   render() {
+    const { message_log } = this.state;
     return (
       <main className="container">
         <h2 className="message-log__header">New Message Log</h2>
@@ -29,13 +40,21 @@ class MessageLogForm extends React.Component {
           <div className="message-log__form">
             <div className="field">
               <label>Messageable Type</label>
-              <select>
+              <select
+                name="messageable_type"
+                value={message_log.messageable_type}
+                onChange={this.handleChange}>
+                <option value="" disabled>Select Messageable Type</option>
                 {MESSAGEABLE_TYPES.map((type) => <option key={type.value} value={type.value}>{type.display}</option>)}
               </select>
             </div>
             <div>
               <label>Messageable ID</label>
-              <select>
+              <select
+                name="messageable_id"
+                value={message_log.messageable_id}
+                onChange={this.handleChange}>
+                <option value="" disabled>Select Messageable ID</option>
                 <option key="1" value="1">1</option>
                 <option key="2" value="2">2</option>
                 <option key="3" value="3">3</option>
@@ -44,25 +63,56 @@ class MessageLogForm extends React.Component {
           </div>
           <div class="field">
             <label>Content</label>
-            <input type="text" id="content"></input>
+            <input
+              type="text"
+              id="content"
+              name="content"
+              value={message_log.content}
+              onChange={this.handleChange} />
           </div>
           <div class="field">
             <label>Delivery Type</label>
-            <input type="text" id="delivery_type"></input>
+            <input
+              type="text"
+              id="delivery_type"
+              name="delivery_type"
+              value={message_log.delivery_type}
+              onChange={this.handleChange} />
           </div>
           <div class="field">
             <label>Delivery Status</label>
-            <input type="text" id="delivery_status"></input>
+            <input
+              type="text"
+              id="delivery_status"
+              name="delivery_status"
+              value={message_log.delivery_status}
+              onChange={this.handleChange} />
           </div>
           <div class="field">
             <label>Sent To</label>
-            <input type="text" id="sent_to_id"></input>
+            <input
+              type="text"
+              id="sent_to_id"
+              name="sent_to_id"
+              value={message_log.sent_to_id}
+              onChange={this.handleChange} />
           </div>
           <div class="field">
             <label>Sent By</label>
-            <input type="text" id="sent_by_id"></input>
+            <input
+              type="text"
+              id="sent_by_id"
+              name="sent_by_id"
+              value={message_log.sent_by_id}
+              onChange={this.handleChange} />
           </div>
-          <button type="submit" value="submit">Submit</button>
+          <button
+            type="submit"
+            value="submit"
+            onClick={() => {
+              // TODO: AJAX Call
+              console.log(this.state.message_log)
+            }}>Submit</button>
         </section>
       </main>
     )
