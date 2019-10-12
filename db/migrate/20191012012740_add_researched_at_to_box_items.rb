7cbb544b7ea374dd6328b4acf48611c6cb65c407
amyhenning <henning.amy@gmail.com>
2019-10-11 20:29:46 -0500

    Add column to box_items table

---
+++ db/migrate/20191012012740_add_researched_at_to_box_items.rb
@@ -0,0 +1,5 @@
+class AddResearchedAtToBoxItems < ActiveRecord::Migration[5.2]
+  def change
+  	add_column :box_items, :researched_at, :datetime
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_10_204858) do
+ActiveRecord::Schema.define(version: 2019_10_12_012740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2019_10_10_204858) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "inventory_type_id", null: false
+    t.datetime "researched_at"
     t.index ["box_id"], name: "index_box_items_on_box_id"
     t.index ["created_by_id"], name: "index_box_items_on_created_by_id"
     t.index ["inventory_type_id"], name: "index_box_items_on_inventory_type_id"
