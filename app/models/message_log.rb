103d8eb22547f2c807bae39b43cf31f97af408f7
Sherilyn Moline <smoline@gmail.com>
2019-10-12 10:17:02 -0400

    sendable dropdowns added to message_log new

--- app/models/message_log.rb
+++ app/models/message_log.rb
@@ -1,7 +1,7 @@
 class MessageLog < ApplicationRecord
   belongs_to :messageable, :polymorphic => true
+  belongs_to :sendable, :polymorphic => true
   belongs_to :sent_by, class_name: "User", foreign_key: :sent_by_id, inverse_of: :message_logs_sent
-  belongs_to :sent_to, class_name: "User", foreign_key: :sent_to_id, inverse_of: :message_logs_received
 
   scope :ordered, -> { order(created_at: :desc) }
 
