edc780bebb94728cd8c5bf01d0504e8064a7d11a
Sherilyn Moline <smoline@gmail.com>
2019-10-12 08:29:10 -0400

    removed messageable since these models are not being used directly

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  include Messageable
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
