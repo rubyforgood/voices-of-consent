501e522b1302d3ad8c219eb425d614d103597574
Mauricio P <mauriciopdvg@gmail.com>
2019-10-23 16:14:50 -0300

    Move the shoulda configs to spec/support/shoulda.rb and order the locations by name in box form

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,7 @@ require 'rspec/rails'
 
 require_relative 'support/controller_macros'
 require_relative 'support/authentication_helpers'
+require_relative 'support/shoulda.rb'
 
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'capybara/rspec'
@@ -37,14 +38,7 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
-RSpec.configure do |config|
-  Shoulda::Matchers.configure do |config|
-    config.integrate do |with|
-      with.test_framework :rspec
-      with.library :rails
-    end
-  end
-  
+RSpec.configure do |config|  
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
