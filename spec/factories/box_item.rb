fe377d4a832c15bff0c746a824891a7416ed7dda
RachelWyatt <rwyatt@taphere.com>
2020-03-07 09:59:29 -0500

    add research_needed and no_research_needed traits to box_item factory

--- spec/factories/box_item.rb
+++ spec/factories/box_item.rb
@@ -4,5 +4,14 @@ FactoryBot.define do
     inventory_type
     created_by
     updated_by
+
+    trait :research_needed do
+      inventory_type {create(:inventory_type, :needs_research)}
+    end
+
+    trait :no_research_needed do
+      inventory_type {create(:inventory_type, :no_research)}
+    end
+    
   end
 end
