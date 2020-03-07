241bff30f0d46c76ea3e20cb430e7e0094a556b9
RachelWyatt <rwyatt@taphere.com>
2020-03-07 10:00:43 -0500

    Add traits to box factory
    Box can now be created with following traits:
    - Reviewed, Design in Progress, Designed, Research in Progress,
    Researched, Assembly in Progress, Assembled, Shipping in Progress,
    Shipped, Follow Up in Progress, Followed Up

--- spec/factories/box.rb
+++ spec/factories/box.rb
@@ -2,15 +2,68 @@ FactoryBot.define do
   factory :box do
     box_request
 
-    trait :is_designed do
-      designed_by {create(:user, :designer)}
+    trait :reviewed do
+      box_request {(create(:box_request, :review_complete))}
+    end
+
+    trait :design_in_progress do 
+      reviewed
+      aasm_state { "design_in_progress"}
+      designed_by_id {create(:user, :designer).id}
       designed_at { Time.now }
       design_reviewed_by_id  {create(:user, :reviewer).id}
       design_reviewed_at { Time.now }
-      #how to update state?
     end
+
+    trait :designed do 
+      design_in_progress
+      aasm_state { "designed"}
+      box_items {create_list(:box_item, 1, :research_needed)}
+    end
+
+    trait :research_in_progress do 
+      designed
+      aasm_state {"research_in_progress"}
+      researched_by_id {create(:user, :researcher).id}
+    end
+
+    trait :researched do 
+      research_in_progress
+      aasm_state {"researched"}
+    end
+
+    trait :assembly_in_progress do 
+      researched
+      aasm_state {"assembly_in_progress"}
+      assembled_by_id {create(:user, :assembler).id}
+    end
+
+    trait :assembled do 
+      assembly_in_progress
+      aasm_state {"assembled"}
+    end
+
+    trait :shipping_in_progress do 
+      assembled
+      aasm_state {"shipping_in_progress"}
+      shipped_by_id {create(:user, :shipper).id}
+    end
+
+    trait :shipped do 
+      shipping_in_progress
+      aasm_state {"shipped"}
+    end
+
+    trait :follow_up_in_progress do 
+      shipping_in_progress
+      aasm_state {"follow_up_in_progress"}
+      followed_up_by_id {create(:user, :follow_upper).id}
+    end
+
+    trait :followed_up do 
+      follow_up_in_progress
+      aasm_state {"followed_up"}
+    end
+
   end
 end
-
-
-#assembled_box = create(:box, :is_assembled), which also has/mimics the traits :is_designed, :is_researched, box_request#is_reviewed, etc, bc our /box_requests #index needs all the prior lifecycle phase to be true and all future lifecycle phases to be untrue to display correctly.
