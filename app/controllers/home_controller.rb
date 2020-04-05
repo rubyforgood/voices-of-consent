c26ccfc647e6b1d4966c0c9eb63bde6003dd18de
Renata Marques <renata.nrmarques@gmail.com>
2020-04-05 18:54:54 -0300

    filter function added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,15 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [:contact]
 
   def index
+    filters = {}
+
+    filters.tap do |hash|
+      permitted_params[:inventory].each do |key, value|
+        filters[key.to_sym] = value if value.present?
+      end
+    end
+
+    @presenter = ::InventoryDashboardPresenter.new(**filters)
   end
 
   def contact
@@ -10,5 +19,11 @@ class HomeController < ApplicationController
 
   def admin
     render :admin, layout: 'application_sidebar'
+  end
+
+  private
+
+  def permitted_params
+    params.permit(inventory: [:start_date, :end_date])
   end
 end
