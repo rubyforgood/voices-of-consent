e3dda3d637e9adef871d2e52e9a49f24e2d5d8cf
Renata Marques <renata.nrmarques@gmail.com>
2020-04-15 23:21:02 -0300

    specs for service ad presenter

---
+++ spec/services/inventory_date_filter_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe InventoryDateFilter do
+  describe "calls the correct method" do
+    let(:hash) {{ inventory: { start_date: Date.today.to_s, end_date: Date.today.to_s } }}
+    let(:hash_expected) { { start_date: Date.today.to_s, end_date: Date.today.to_s } }
+
+    context "no params given" do
+      it "#initialize" do
+        expect(subject.params).to eq({})
+      end
+
+      it ".call" do
+        expect(described_class.call).to eq({})
+      end
+
+      it "#call" do
+        expect(described_class.new.call).to eq({})
+      end
+    end
+
+    context "params given" do
+      it "#initialize" do
+        expect(described_class.new(hash).params).to eq(hash)
+      end
+
+      it "#call" do
+        expect(described_class.new(hash).call).to eq(hash_expected)
+      end
+      it "#set_value_for_inventory" do
+        expect(described_class.new(hash).set_value_for_inventory(hash)).to eq(hash_expected)
+      end
+      it ".call" do
+        expect(described_class.call(hash)).to eq(hash_expected)
+      end
+    end
+  end
+end
