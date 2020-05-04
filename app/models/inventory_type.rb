8dc97ae791f3a65e22062ad106b792fee0d06f54
Julia Furlani <julia.furlani@codeminer42.com>
2020-05-04 12:19:42 -0300

    creating an after_create callback on inventory_type to create tallies on all locations that are storage units

--- app/models/inventory_type.rb
+++ app/models/inventory_type.rb
@@ -2,4 +2,14 @@ class InventoryType < ApplicationRecord
   has_many :box_items
   has_many :core_box_items
   has_many :inventory_tallies
+
+  after_create :create_inventory_tallies
+
+  private
+
+  def create_inventory_tallies
+    Location.all.each do |location|
+      inventory_tallies.create(storage_location: location) if location.storage_unit?
+    end
+  end
 end
